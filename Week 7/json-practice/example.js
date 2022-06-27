d45cea2309edeecb676a3688ce9039e49be21f01
winterborn <phwhite89@gmail.com>
2022-06-27 21:04:23 +0100

    added json project

---
+++ Week 7/json-practice/example.js
@@ -0,0 +1,12 @@
+let person = {
+    "name": "Phil",
+    "pets": [{
+            "species": "dog",
+            "name": "Ella"
+        }
+    ],
+    "favouriteFoods": ["noodles", "steak",
+        "lemon cheesecake"
+    ]
+}
+console.log(person);
---
+++ Week 7/json-practice/main.js
@@ -0,0 +1,14 @@
+// Create an object regarding you with key value pairs, saved within variable
+
+let person = {
+
+    firstName: "Phil",
+    lastName: "White",
+    gender: "male",
+    city: "London"
+
+}
+
+console.log(person);
+// console.log(person.firstName);
+// console.log(person.firstName + person.lastName);
