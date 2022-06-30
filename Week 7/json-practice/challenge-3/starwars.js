72f4602e20a31879abe5ef7878380273e1665ca0
winterborn <phwhite89@gmail.com>
2022-06-30 16:50:26 +0100

    updated challenge 2; challenge 3 - sw api call

---
+++ Week 7/json-practice/challenge-3/starwars.js
@@ -0,0 +1,33 @@
+
+const fetch_btn = document.getElementById('myBtn');
+const sw_result = document.getElementById('beautify');
+fetch_btn.addEventListener('click', getData);
+
+
+function getData() {
+
+    url = "https://swapi.dev/api/people/";
+    fetch(url).then((Response) => {
+            console.log("Promise done");
+            return Response.json();
+        })
+
+        .then((data) => {
+            console.log("After getting data");
+            console.log(data.results);
+
+            // loop through array objects
+            for (let i = 0; i < data.results.length; i++) {
+                let obj = data.results[i];
+
+                console.log(obj.name);
+                // sw_result.innerHTML += (obj.name + "<br>");
+                sw_result.innerHTML += (obj.name + " " + " | " + " " + obj.birth_year + "<br>");
+
+            }
+        });
+
+}
+
+console.log("Before fetching data");
+getData();
