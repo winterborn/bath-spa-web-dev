ef5c115d378571c76c279349de27c4f89a1dea68
winterborn <phwhite89@gmail.com>
2022-07-04 22:34:27 +0100

    reactjs set up

---
+++ Week 8/reactjs/reactjs-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hey all, this is my first edit on react
+        </p>
+        <a>
+          learn react with heart
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
