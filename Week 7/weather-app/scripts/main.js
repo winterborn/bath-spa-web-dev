e8848748e345553236cd17f5131514a8b521a308
winterborn <phwhite89@gmail.com>
2022-07-07 18:48:14 +0100

    updated scripts

--- Week 7/weather-app/scripts/main.js
+++ Week 7/weather-app/scripts/main.js
@@ -79,11 +79,11 @@ function showWeatherData(data) {
     </div>
     <div class="weather-item">
         <div>Sunrise</div>
-        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm')}</div>
     </div>
     <div class="weather-item">
         <div>Sunset</div>
-        <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm')}</div>
     </div>`;
 
   let otherDayForecast = ''
